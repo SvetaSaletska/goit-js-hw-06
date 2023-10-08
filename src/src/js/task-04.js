8e531b34e8ab7a103f7b0f777920e5b11c0f3ab9
SvetaSaletska <svitlanagalevich@gmail.com>
2023-10-09 02:09:31 +0300

    Update task-04

---
+++ src/src/js/task-04.js
@@ -0,0 +1,8 @@
+const decrementBtn = document.querySelector('.decrement');
+const incrementBtn = document.querySelector('.increment');
+const valueEL = document.querySelector('#value');
+
+
+
+console.log(valueEL);
+console.log(incrementBtn);
