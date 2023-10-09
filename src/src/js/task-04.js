61479fab2c813e843cfff7db45456638c81f5783
SvetaSaletska <svitlanagalevich@gmail.com>
2023-10-09 23:28:04 +0300

    task-04-done

--- src/src/js/task-04.js
+++ src/src/js/task-04.js
@@ -1,8 +1,28 @@
-const decrementBtn = document.querySelector('.decrement');
-const incrementBtn = document.querySelector('.increment');
+const counter = {
+    counterValue: 0,
+    increment() {
+        this.counterValue += 1;
+    },
+    decrement() {
+        this.counterValue -= 1;
+    }
+}
+
+const decrementBtn = document.querySelector('button[data-action="decrement"]');
+const incrementBtn = document.querySelector('button[data-action="increment"]');
 const valueEL = document.querySelector('#value');
 
+decrementBtn.addEventListener('click', function () {
+    counter.decrement();
+    valueEL.textContent = counter.counterValue;
+})
+
+incrementBtn.addEventListener('click', function () {
+    counter.increment();
+    valueEL.textContent = counter.counterValue;
+})
 
 
 console.log(valueEL);
+console.log(decrementBtn);
 console.log(incrementBtn);
