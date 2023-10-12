20455933ed5f41e635f237c4b7812b8e797d5d3b
SvetaSaletska <svitlanagalevich@gmail.com>
2023-10-12 23:41:59 +0300

    task-09-done

--- src/src/js/task-09.js
+++ src/src/js/task-09.js
@@ -3,3 +3,17 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+
+const body = document.querySelector('body');
+const color = document.querySelector('.color')
+const changeColorbtn = document.querySelector('.change-color');
+
+
+changeColorbtn.addEventListener('click', handlerClick);
+
+function handlerClick(evt) {
+let randClr = getRandomHexColor();
+  body.style.backgroundColor = randClr;
+  color.getRandomHexColor = randClr;
+}
