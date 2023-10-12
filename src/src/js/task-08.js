07a9458395b076b45bc2866d89b89717940060c4
SvetaSaletska <svitlanagalevich@gmail.com>
2023-10-12 15:37:19 +0300

    update task-08

--- src/src/js/task-08.js
+++ src/src/js/task-08.js
@@ -1,1 +1,16 @@
+const form = document.querySelector('.login-form');
 
+form.addEventListener('submit', handlerSubmit);
+
+function handlerSubmit(evt) {
+   evt.preventDefault();
+
+   const {email, password} = evt.currentTarget.elements;
+ 
+   const data = {
+      email: email.value,
+      password: password.value
+   }
+
+   evt.currentTarget.reset();
+}
